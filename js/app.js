16e64ce45e13c6189efb3c97a26377f3c5045d1b
LedentLoric <loric.ledent@gmail.com>
2018-06-28 16:33:02 +0200

    Menu overlay animation

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-	// Hamberger Menu
+	// Hamburger Menu
 	document.querySelector('.header-hamburger-menu').addEventListener('click', function() {
 		this.querySelector('.header-hamburger-icon').classList.toggle("open");
+
+		let menuContainer = document.querySelector('.header-menu-overlay');
+		
+		let screenDiagonal = 2 * Math.sqrt(Math.pow(document.body.offsetWidth, 2) + Math.pow(document.body.offsetHeight, 2));
+		menuContainer.style.width = screenDiagonal + 'px';
+		menuContainer.style.height = screenDiagonal + 'px';
+
+		menuContainer.classList.toggle('header-menu-overlay-open');
 	});
 });
