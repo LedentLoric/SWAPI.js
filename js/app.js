b57d599554f4fdef0cf39d21373baa3188033343
LedentLoric <loric.ledent@gmail.com>
2018-07-02 15:02:15 +0200

    Display menu items

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-	
+
 	// Hamburger Menu
 	document.querySelector('.header-hamburger-menu').addEventListener('click', function() {
 		this.querySelector('.header-hamburger-icon').classList.toggle("open");
 
-		let menuContainer = document.querySelector('.header-menu-overlay');
+		let menuOverlay = document.querySelector('.header-menu-overlay');
 		
 		let screenDiagonal = 2 * Math.sqrt(Math.pow(document.body.offsetWidth, 2) + Math.pow(document.body.offsetHeight, 2));
-		menuContainer.style.width = screenDiagonal + 'px';
-		menuContainer.style.height = screenDiagonal + 'px';
+		menuOverlay.style.width = screenDiagonal + 'px';
+		menuOverlay.style.height = screenDiagonal + 'px';
 
-		menuContainer.classList.toggle('header-menu-overlay-open');
+		menuOverlay.classList.toggle('header-menu-overlay-open');
+
+		let menuContainer = document.querySelector('.header-menu-container');
+		menuContainer.classList.toggle('header-menu-container-open');
+		document.querySelector('.header-title').classList.toggle('header-title-hidden');
 	});
 });
