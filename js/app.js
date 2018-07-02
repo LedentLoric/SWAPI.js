e37c35bfb227247133e9a27de1141a06f38d7afd
LedentLoric <loric.ledent@gmail.com>
2018-07-02 16:49:10 +0200

    Start films display

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
+// AJAX REQUESTS
+function makeAjaxRequest(url, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url);
+	xhr.responseType = "json";
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhr.onload = function() {
+	    if (xhr.status === 200) {
+	        callback(xhr.response);
+	    }
+	    // else {
+	    //     reject('Request failed.  Returned status of ' + xhr.status);
+	    // }
+	};
+	xhr.send();
+	
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 
 	// Hamburger Menu
@@ -15,5 +33,26 @@ document.addEventListener('DOMContentLoaded', function() {
 		let menuContainer = document.querySelector('.header-menu-container');
 		menuContainer.classList.toggle('header-menu-container-open');
 		document.querySelector('.header-title').classList.toggle('header-title-hidden');
+	});
+
+	// Films
+	makeAjaxRequest('https://swapi.co/api/films/', function(data) {
+		let films = data.results;
+
+		films.sort(function(a, b) {
+			return a.episode_id - b.episode_id;
+		});
+
+		let filmsSection = document.querySelector('.films-section');
+
+		films.forEach(function(film, index) {
+			let filmElement = document.createElement('div');
+			let htmlContent = 
+				'<div class="film-container">' +
+				'<img src="assets/images/film-' + film.episode_id + '.jpeg"/>' +
+				'</div>';
+			filmElement.innerHTML = htmlContent;
+			filmsSection.appendChild(filmElement.firstChild);
+		});
 	});
 });
